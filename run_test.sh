05590e94c2f037fbb463eb820826606bc657db02
TheAssembler1 <lewisnoah592@gmail.com>
2025-08-31 09:25:06 -0400

    shorten test names

--- run_test.sh
+++ run_test.sh
@@ -8,6 +8,6 @@ echo "killing previous pdc_server"
 pkill -f pdc_server || true
 
 pushd ./build
-#ctest -V -L transform --output-on-failure
-ctest --output-on-failure
+ctest -V -L transform --output-on-failure
+#ctest --output-on-failure
 popd
