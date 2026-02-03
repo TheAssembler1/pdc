931e8b89e7b929e94d9bc7c0f15d9f738cbd8648
Noah Lane Lewis <nlewi26@login01.chn.perlmutter.nersc.gov>
2026-02-03 15:50:43 -0800

    fix vpic tests bug

--- src/tests/misc/vpicio_mts.c
+++ src/tests/misc/vpicio_mts.c
@@ -183,32 +183,25 @@ main(int argc, char **argv)
                 LOG_ERROR("Error getting an object id of %s from server\n", obj_name);
                 return FAIL;
             }
-            if (!strcmp(transformation_str, "turbo")) {
-                LOG_DEBUG("TURBO TRANSFORMATION");
-                if (i >= 6) {
-                    pdcid_t dg_id = PDCtf_dg_json_create(TF_GRAPHS_DIR "turbo.json");
-                    PDCtf_attach_to_obj(dg_id, obj_ids[i], "decompressed", "compressed");
-                }
-                else {
-                    pdcid_t dg_id = PDCtf_dg_json_create(TF_GRAPHS_DIR "zfp_gpu.json");
-                    PDCtf_attach_to_obj(dg_id, obj_ids[i], "decompressed", "compressed");
-                }
+            if (!strcmp(transformation_str, "turbo") && obj_prop == obj_prop_int) {
+		LOG_WARNING("Attaching turbo to index: %d\n", obj_ids[i]);
+                pdcid_t dg_id = PDCtf_dg_json_create(TF_GRAPHS_DIR "turbo.json");
+                PDCtf_attach_to_obj(dg_id, obj_ids[i], "decompressed", "compressed");
             }
-            else if (!strcmp(transformation_str, "zfp")) {
+            else if (!strcmp(transformation_str, "zfp") && obj_prop == obj_prop_float) {
+		LOG_WARNING("Attaching zfp to index: %d\n", obj_ids[i]);
                 pdcid_t dg_id = PDCtf_dg_json_create(TF_GRAPHS_DIR "zfp.json");
                 PDCtf_attach_to_obj(dg_id, obj_ids[i], "decompressed", "compressed");
             }
-            else if (!strcmp(transformation_str, "zfp_gpu")) {
+            else if (!strcmp(transformation_str, "zfp_gpu") && obj_prop == obj_prop_float) {
+		LOG_WARNING("Attaching zfp gpu to index: %d\n", obj_ids[i]);
                 pdcid_t dg_id = PDCtf_dg_json_create(TF_GRAPHS_DIR "zfp_gpu.json");
                 PDCtf_attach_to_obj(dg_id, obj_ids[i], "decompressed", "compressed");
             }
-            else if (!strcmp(transformation_str, "sz")) {
+            else if (!strcmp(transformation_str, "sz") && obj_prop == obj_prop_float) {
+		LOG_WARNING("Attaching sz to index: %d\n", obj_ids[i]);
                 pdcid_t dg_id = PDCtf_dg_json_create(TF_GRAPHS_DIR "sz.json");
                 PDCtf_attach_to_obj(dg_id, obj_ids[i], "decompressed", "compressed");
-            }
-            else if (strcmp(transformation_str, "raw")) {
-                print_usage();
-                abort();
             }
         }
 
