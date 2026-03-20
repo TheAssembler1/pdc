af3b17a120920974c2ee7402293cbf418ccbed5e
Noah Lane Lewis <nlewi26@login20.chn.perlmutter.nersc.gov>
2026-03-20 13:27:07 -0700

    checkpoint

--- src/server/pdc_server.c
+++ src/server/pdc_server.c
@@ -2166,11 +2166,11 @@ PDC_Server_loop(hg_context_t *hg_context)
         }
 #endif
 
-        if (pdc_tf_update_profiler() != SUCCEED) {
+        /*if (pdc_tf_update_profiler() != SUCCEED) {
             LOG_ERROR("Error updating profiler\n");
             ret_value = FAIL;
             break;
-        }
+        }*/
 
         actual_count = 0;
         do {
